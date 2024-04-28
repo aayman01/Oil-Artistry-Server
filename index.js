53814efecd96c9ede62c239a3b505a504fdc4445
aayman01 <ayman.alfi098@gmail.com>
2024-04-28 11:43:09 +0600

    getting data by specific id

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ async function run() {
       res.send(result);
     })
     
+    app.get("/artcraft/:id", async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await artsAndCraftCollection.find(query).toArray();
+      res.send(result)
+    })
+
     app.get("/mycraft/:email", async(req, res)=> {
       const email = req.params.email;
       const query = { user_email: email };
